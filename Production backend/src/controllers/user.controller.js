85644edd69c581798794d165e8210b136dc5d566
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-11-14 01:48:44 +0530

    accessing data from postman to check all good/

--- Production backend/src/controllers/user.controller.js
+++ Production backend/src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res, next, err) => {
-   return res.status(200).json({
-        message: "chai aur code"
-    })
+    //get user details from frontend.
+    // validation-not empty
+    //check user exist : username or email
+    // check images or avatar
+    //upload on cloudinary,avatar
+    // create user-object- create entry in db
+    //remove password and refresh token from response
+    // check for user creation
+    // return res 
+
+
+    const {fullName,email,username,password}=req.body
+    console.log("fullname:",fullName);
+    console.log("email",email);
+    console.log("username",username);
+    console.log("password:",password);
+    
 })
 
 export {
