d748bcc7944b8a578f6b7a80940f6fc003675f64
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-11-15 00:43:43 +0530

    user auth login,logout session .

--- Production backend/src/controllers/user.controller.js
+++ Production backend/src/controllers/user.controller.js
@@ -144,27 +144,53 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const loggedInUser = await user.findById(user._id).select("-password -refreshToken")
 
-    const options={
-        httpOnly:true,
-        secure:true,
+    const options = {
+        httpOnly: true,
+        secure: true,
     }
     return res
-    .status(200)
-    .cookie("accessTokens:",accessToken,options)
-    .cookie("refreshToken:",refreshToken,options)
-    .json(
-        new ApiResponse(
-            200,{
-                user:loggedInUser,accessToken,refreshToken
+        .status(200)
+        .cookie("accessTokens:", accessToken, options)
+        .cookie("refreshToken:", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200, {
+                user: loggedInUser, accessToken, refreshToken
             },
-            "User Logged In Successfully"
+                "User Logged In Successfully"
+            )
         )
+
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+        req.user._id
+        ,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
     )
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
 
-
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200,{},"user logged out "))
 })
 
 
 export {
     registerUser,
+    loginUser,
+    logoutUser
 }
