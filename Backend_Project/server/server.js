3d46951d9452a29b7709c4c06c65bc93b8fa8458
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-02-13 01:22:29 +0530

    correct port

--- Backend_Project/server/server.js
+++ Backend_Project/server/server.js
@@ -25,7 +25,7 @@ app.get('/',(req,res)=>{
     res.send("Server is Running 👍🏼");
 });
 
-const PORT=process.env.PORT ||5050;
+const PORT=process.env.PORT ||5000;
 
 app.listen(PORT,()=>{
     console.log(` ✅ Server is running on PORT ${PORT}`);
