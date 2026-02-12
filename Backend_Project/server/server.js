e382e6449ea30dbd16244c6509540b473756f59b
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-02-12 23:09:39 +0530

    adding routes in server.js.

--- Backend_Project/server/server.js
+++ Backend_Project/server/server.js
@@ -2,21 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config();
 
 const app=express();
-
-const PORT=process.env.PORT ||5050;
 
 app.use(cors());
 app.use(express.json());
 
 connectDB();
 
+app.use("/api/auth",authRoutes);
+
 app.get('/',(req,res)=>{
     res.send("Server is Running 👍🏼");
 });
+
+const PORT=process.env.PORT ||5050;
 
 app.listen(PORT,()=>{
     console.log(` ✅ Server is running on PORT ${PORT}`);
