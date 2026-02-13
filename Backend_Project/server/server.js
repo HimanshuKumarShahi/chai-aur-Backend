533c64f60bffe3768752fd590de5054fa6fa8c85
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-02-14 01:52:40 +0530

    correct port

--- Backend_Project/server/server.js
+++ Backend_Project/server/server.js
@@ -13,13 +13,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://bankingsystem-seven.vercel.app"
+  ],
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -45,4 +48,3 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-export default app;
