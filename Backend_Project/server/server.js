55f0da1daccd1e7cddacacc8a1c039c26abcf83c
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-02-14 02:18:35 +0530

    done ✅

--- Backend_Project/server/server.js
+++ Backend_Project/server/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bankingsystem-seven.vercel.app",
+      "https://chai-aur-backend-jrmt.onrender.com",
     ],
     credentials: true,
   })
