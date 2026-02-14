0cc71579c4d1ec4ae2ef81e06f9460a92f498af0
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-02-14 17:23:19 +0530

    done.

--- Backend_Project/client/src/api/api.js
+++ Backend_Project/client/src/api/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
+
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: false,
-=======
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
->>>>>>> 3775c8ac4dcc2da0ce0d2e59612f620805103cb9
 });
 
 api.interceptors.request.use((config) => {
