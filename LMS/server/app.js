5f66f05af93ed1c20a88a9da34c7e7f1cf1b988f
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-03-14 22:34:29 +0530

    Debuging errors

--- LMS/server/app.js
+++ LMS/server/app.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import userRoutes from './routes/user.route.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app=express();
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
 
 app.use(cors({
-    credentials: true
+  origin: "http://localhost:5173",
+  credentials: true
 }));
-app.use(cookieParser())
-app.use(express.json())
 
-app.use('/api/auth',userRoutes)
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes)
 
 export default app;
