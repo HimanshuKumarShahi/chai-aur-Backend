f7b3c15e948be1beea4ea18cb28967ab4224a93d
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-03-26 13:00:28 +0530

    delete it

--- MobileApps/blink-kart/backend/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const protect = (req, res, next) => {
-  const user = req.headers["x-user-id"];
-  if (!user) return res.status(401).json({ msg: "Unauthorized" });
-  req.user = user;
-  next();
-};
--- MobileApps/blink-kart/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  clerkId: String,
-  email: String,
-  name: String,
-});
-
-export default mongoose.model("User", userSchema);
