dd0d90e397281dab0b879e1f47159d14148a4bda
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-03-27 20:27:03 +0530

    Delete all the files.

--- lms_mobile/server/src/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-export const isAdmin = (req, res, next) => {
-  if (req.auth.userId === process.env.ADMIN_CLERK_ID) {
-    next();
-  } else {
-    res.status(403).json({ message: "Not authorized" });
-  }
-};
--- lms_mobile/server/src/routes/enrollRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { enrollCourse } from '../controllers/enrollController.js';
-import { requireAuth } from '@clerk/express';
-
-const router = express.Router();
-
-router.post('/', requireAuth(), enrollCourse);
-
-export default router;
