c463c0c2e75461e3f4bb13c89179ff5f6c7a1ab6
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-16 15:40:32 +0530

    remove other project backend

--- MobileApps/server/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-export const requireAdmin = (req, res, next) => {
-  const adminId = process.env.ADMIN_CLERK_ID;
-
-  if (!req.auth || req.auth.userId !== adminId) {
-    return res.status(403).json({ message: "Admin only access" });
-  }
-
-  next();
-};
